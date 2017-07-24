9208d926c7ec16bf4380abc5c51891eeaa6c66e2
dangkhoa <peterkhoa2008@gmail.com>
2017-07-24 20:47:24 +0700

    Update tocart.php

--- tocart.php
+++ tocart.php
@@ -3,6 +3,7 @@
 	session_start();
 		if ($_SERVER["REQUEST_METHOD"]=="POST"){
 			if(!empty($_POST)){
+				//Can check empty ở đoạn này, trước khi gắn session.
 				$_SESSION["cost"][]=$_POST["cost"];
 				$_SESSION["prname"][]=$_POST["prname"];
 				$_SESSION["total"]+=intval($_POST["cost"]);
